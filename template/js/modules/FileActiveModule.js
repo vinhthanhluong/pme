0d7cf99fc28a24f27c882602dd4c816e78f47f26
thành <vinhthanhluong@gmail.com>
2022-01-19 18:04:20 +0700

    update code 12 page

--- template/js/modules/FileActiveModule.js
+++ template/js/modules/FileActiveModule.js
@@ -4,4 +4,11 @@ export default function FileActiveModule() {
     $('.tab-link-mobi').on('click', function () {
         $('.tab-link').slideToggle();
     });
+
+    if ($('.question').length) {
+        $('.q-title').on('click', function () {
+            $(this).parent().toggleClass('active');
+            $(this).next('.q-content').slideToggle();
+        });
+    }
 }
