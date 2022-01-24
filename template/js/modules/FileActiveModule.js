2a27c5e08b09bf6d60a65cfd051f371c3bf97639
thành <vinhthanhluong@gmail.com>
2022-01-24 14:21:59 +0700

    update first pme

--- template/js/modules/FileActiveModule.js
+++ template/js/modules/FileActiveModule.js
@@ -1,5 +1,5 @@
 export default function FileActiveModule() {
-  
+
 
     $('.tab-link-mobi').on('click', function () {
         $('.tab-link').slideToggle();
@@ -12,18 +12,18 @@ export default function FileActiveModule() {
         });
     }
 
-    if ($(window).width() <  768) {
+    if ($(window).width() < 768) {
         $('.home-intros .swiper').addClass('auto-height');
     }
 
     if ($('.bx-video').length) {
         let videos = document.querySelectorAll('.s-video');
 
-        for(let video of videos){
-            video.onplay = function() {
+        for (let video of videos) {
+            video.onplay = function () {
                 video.parentElement.classList.add('playing');
             }
-            video.onpause = function() {
+            video.onpause = function () {
                 video.parentElement.classList.remove('playing');
             }
         }
@@ -33,6 +33,12 @@ export default function FileActiveModule() {
         $('.v-title').on('click', function () {
             $(this).parent('.vd-item').toggleClass('active');
         });
+    }
+
+
+    if ($('.signup-login').length && $(window).width() <= 576) {
+        $('.f-act .f-submit').appendTo('.f-act');
+
     }
 
 }
